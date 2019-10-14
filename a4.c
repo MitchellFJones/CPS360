cfbf34bafd7f570868c8ed0113cfced9af3c9557
Mitchell Jones <32419983+MitchellFJones@users.noreply.github.com>
2019-10-14 13:17:11 -0400

    a4
    Using digital logic circuits within the construct of a C-Program

---
+++ a4.c
@@ -0,0 +1,123 @@
+/*
+Name: Mitchell Jones & Brandon Stolz        Class: CPS 360
+Assignment: 4
+Due: Oct 16, 2019                                           
+Points: 10
+
+Problem:
+
+Solution:
+
+Data-Structure Used:
+
+Errors-Handled:
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+/**/
+
+int main(int argc, char *argv[]){
+    
+    void usage(char *progname);
+    
+    exit (0);
+}
+
+/**/
+
+void usage(char *progname){
+    
+    fprintf(stderr, "./%s <arg-a> <arg-b> <arg-c>\n", progname); exit(1);
+}
+
+/**/
+
+void halfaddr(int a, int b, int *sum, int *outcary){
+    
+    *sum = (a & 1) ^ (b & 1);
+    *outcary = (a & 1) & (b & 1);
+}
+
+/**/
+
+void fulladdr(int a, int b, int incary, int *sum, int *outcary){
+    
+    *sum = (a & 1) ^ (b & 1) ^ (incary & 1);
+    *outcary = (a & b) | (incary & (a | b));
+}
+
+/**/
+
+void rcadd4(int a, int b, int incary, int *sum, int *outcary){
+    
+    int s3, s2, s1, s0;
+    
+    int *s3p = &s3, *s2p = &s2, *s1p = &s1, *s0p = &s0;
+    
+    fulladdr(a & 1, b & 1, incary, s0, outcary);
+    fulladdr((a >> 1) & 1, (b >> 1) & 1, *outcary, s1, outcary);
+    fulladdr((a >> 2) & 1, (b >> 2) & 1, *outcary, s2, outcary);
+    fulladdr((a >> 3) & 1, (b >> 3) & 1, *outcary, s3, outcary);
+    
+    *sum = (s3 << 3) | (s2 << 2) | (s1 << 1) | s0;
+}
+
+/**/
+
+void lacadd4(int a, int b, int incary, int *sum, int *outcary){
+    
+}
+
+/**/
+
+void evenparity3gen(int a, int *paritybit){
+    
+    int b2, b1, b0;
+    
+    b2 = (a >> 2) & 1;
+    b1 = (a >> 1) & 1;
+    b0 = (a & 1);
+    
+    *checkbit = (b2 ^ b1) | b0;
+}
+
+/**/
+
+void oddparity4chk(int a, int *checkbit){
+    
+    int b3, b2, b1, b0;
+    
+    b3 = (a >> 3) & 1;
+    b2 = (a >> 2) & 1;
+    b1 = (a >> 1) & 1;
+    b0 = (a & 1);
+    
+    *checkbit = ((b3 ^ b2) ^ (b1 ^ b0)) ^ 1;
+}
+
+/**/
+
+void mux2by1(int a, int b, int *yout){
+    
+}
+
+/**/
+
+void mux4by1(int a, int b, int *yout){
+    
+}
+
+/**/
+
+void printresults(char *label, int a){
+    
+    printf("%-16s", label);
+    
+    int i, j;
+    for(i = 31, j = 0; i >= 0; i--, j++) {
+        if ((j & 0x7)  == 0) putchar(' ');
+        (a & (1 << i)) ? putchar('1') : putchar('0');
+    }
+    printf("\n");
+}
